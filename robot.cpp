7630d1a5ad3890c8b40eefa0c62e19ab583a28f3
Vidura Sumanasena <vidurayashan@gmail.com>
2020-09-11 02:28:21 +0530

    Print shortest path

--- robot.cpp
+++ robot.cpp
@@ -13,6 +13,7 @@ struct cell{
     int x;
     int y;
     unsigned int time=0;
+    cell * parent = nullptr;
 };
 
 class bfs_tree{
@@ -25,6 +26,17 @@ public:
         this->destination = destination;
         map[ start.y ][start.x] = SALESMAN_1_TRAIL;
         map[ destination.y ][ destination.x ] = SALESMAN_2_TRAIL;
+
+        map_of_parents = new int* [Y];
+	    for (int i=0; i<Y; i++){
+		    map_of_parents[i] = new int[X];
+	    }
+
+        for (int i=0; i<Y; i++){
+            for (int j=0; j<X; j++){
+                map_of_parents[i][j] = 0;
+            }
+        }
     }
 
     void optimized_bfs(){
@@ -34,21 +46,24 @@ public:
         int x2 = destination.x;
         int y2 = destination.y;
 
+        map[y1][x1] = SALESMAN_1_TRAIL;
         bfs_queue.push_back(start);
 
+        map[y2][x2] = SALESMAN_2_TRAIL;
+
         int ret1=-1;
-        int ret2=-1;
 
         while ( bfs_queue.empty() == false ){
         	
-        	cout << "stack 1 :";
-        	for (auto i: bfs_queue){
-        		cout << i.x  << " " << i.y << endl;
-			}
-			cout << endl;
+        	// cout << "stack 1 :";
+        	// for (auto i: bfs_queue){
+        	// 	cout << i.x  << "-" << i.y << endl;
+			// }
+			// cout << endl;
             
             ret1 = push_neighbours_to_bfs_stack(bfs_queue, bfs_queue[0]);
             if (ret1 != -1){
+                print_path();
                 cout << "Answer :" << ret1 << endl;
                 break;
             }
@@ -64,7 +79,38 @@ public:
 			}
 			cout << endl;
 		}
+        cout << endl << endl;
+        for (int i=0; i<Y; i++){
+			for (int j=0; j<X; j++){
+				cout << map_of_parents[i][j] << " ";
+			}
+			cout << endl;
+		}
 
+    }
+
+    int print_path(){
+        cout << "Print path called" << endl;
+        int p = destination.x;
+        int q = destination.y;
+        while(true){
+            cout << "p = " << p << " q = " << q << endl;
+            if ( map_of_parents[q][p] == 0 ){
+                break;
+            }else{
+                map[q][p] = 8;
+                if ( map_of_parents[q][p] == 1 ){
+                    q = q-1;
+                }else if ( map_of_parents[q][p] == 2 ){
+                    p = p-1;
+                }else if ( map_of_parents[q][p] == 3 ){
+                    q = q+1;
+                }else if ( map_of_parents[q][p] == 4){
+                    p = p+1;
+                }
+            }
+
+        }
     }
 
     int push_neighbours_to_bfs_stack( vector<cell>& stck, cell _cell){
@@ -72,6 +118,8 @@ public:
         int ret=-1;
         int x = _cell.x;
         int y = _cell.y;
+
+        cout << "cell: (" << y << "," << x << ")" << endl;
         do{
             if ( y - 1 >= 0 ){
                 if ( map[ y-1 ][x] != WALL ){
@@ -79,11 +127,13 @@ public:
                         tmp.x = x;
                         tmp.y = y-1;
                         tmp.time = _cell.time + 1;
+                        map_of_parents[tmp.y][tmp.x] = 3;
                         stck.push_back(tmp);
                         map[y-1][x] = SALESMAN_1_TRAIL;
                     }else if ( map [y-1][x] == SALESMAN_2_TRAIL){
                         cout << "Soulution found" << endl;
                         ret = _cell.time + 1;
+                        map_of_parents[y-1][x] = 3;
                         break;
                     }
                 }
@@ -94,10 +144,12 @@ public:
                         tmp.x = x-1;
                         tmp.y = y;
                         tmp.time = _cell.time + 1;
+                        map_of_parents[tmp.y][tmp.x] = 4;
                         stck.push_back(tmp);
                         map[y][x-1] = SALESMAN_1_TRAIL;
                     }else if ( map [y][x-1] == SALESMAN_2_TRAIL){
                         cout << "Soulution found" << endl;
+                        map_of_parents[y][x-1] = 4;
                         ret = _cell.time + 1;
                         break;
                     }
@@ -110,10 +162,12 @@ public:
                         tmp.x = x;
                         tmp.y = y+1;
                         tmp.time = _cell.time + 1;
+                        map_of_parents[tmp.y][tmp.x] = 1;
                         stck.push_back(tmp);
                         map[y+1][x] = SALESMAN_1_TRAIL;
                     }else if ( map [y+1][x] == SALESMAN_2_TRAIL){
                         cout << "Soulution found" << endl;
+                        map_of_parents[y+1][x] = 1;
                         ret = _cell.time + 1;
                         break;
                     }
@@ -125,10 +179,12 @@ public:
                         tmp.x = x+1;
                         tmp.y = y;
                         tmp.time = _cell.time + 1;
+                        map_of_parents[tmp.y][tmp.x] = 2;
                         stck.push_back(tmp);
                         map[y][x+1] = SALESMAN_1_TRAIL;
                     }else if ( map [y][x+1] == SALESMAN_2_TRAIL){
                         cout << "Soulution found" << endl;
+                        map_of_parents[y][x+1] = 2;
                         ret = _cell.time + 1;
                         break;
                     }
@@ -139,6 +195,7 @@ public:
     }
 
     int **map;
+    int **map_of_parents;
     int X;
     int Y;
 
